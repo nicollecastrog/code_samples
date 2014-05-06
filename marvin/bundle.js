8e506d942974012615a5cbbbfc8f3646e25f5488
nicollecastrog <nicollecastrog@gmail.com>
2014-05-06 12:19:46 +0100

    Adding browserify

--- marvin/bundle.js
+++ marvin/bundle.js
@@ -1,3 +1,4 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"Focm2+":[function(require,module,exports){
 var G = {};
 
 G.getInput = function getInput (inputValue) {
@@ -65,3 +66,7 @@ module.exports = G;
 
 
 
+
+},{}],"./index.js":[function(require,module,exports){
+module.exports=require('Focm2+');
+},{}]},{},[])
